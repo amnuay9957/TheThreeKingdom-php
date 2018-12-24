c4525b164e840d5419abb9b4098f12a4a9391680
amnuay9957 <46090160+amnuay9957@users.noreply.github.com>
2018-12-24 21:04:23 +0700

    Add files via upload

---
+++ Kingdom.php
@@ -0,0 +1,27 @@
+<?php
+    $accessToken = "Z4L94grrat+Mj/EDYW/ANosMWLRkvLYeW2v/kd+j7v3JvdMd0hpw09B6JiAAjraxqBCd5taYXo/mhPnH8GV3Fh7O+ctXbtsCxTYs2qW/SYMSb740M87VDCkdP0zzUHSPFcsCUIz9Y7KygGKHThn30QdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+
+    //รับข้อความจากผู้ใช้
+    $message = $arrayJson['events'][0]['message']['text'];
+#ตัวอย่าง Message Type "Text"
+    if($message == "สวัสดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
+    #ตัวอย่าง Message Type "Sticker"
+    else if($message == "ฝันดี"){
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "sticker";
+        $arrayPostData['messages'][0]['packageId'] = "2";
+        $arrayPostData['messages'][0]['stickerId'] = "46";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
